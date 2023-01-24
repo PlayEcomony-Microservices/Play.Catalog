5128bb7832a11c50488b0986b725f11a3c166e21
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-01-24 20:06:36 +0300

    register mongoclient and itemsrepository to iserviceprovider for dependency injection

--- src/Play.Catalog.Service/Program.cs
+++ src/Play.Catalog.Service/Program.cs
@@ -2,13 +2,30 @@ using Microsoft.OpenApi.Models;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using Play.Catalog.Service.Repositories;
+using Play.Catalog.Service.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
+var Configuration = builder.Configuration;
 
 // Add services to the container.
 
+ServiceSettings serviceSettings;
+
 BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
 BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+serviceSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+builder.Services.AddSingleton(serviceProvider => 
+{
+    var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+    var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
+    return mongoClient.GetDatabase(serviceSettings.ServiceName);
+});
+
+builder.Services.AddSingleton<IItemsRepository, ItemsRepository>();
 
 builder.Services.AddControllers(options =>
 {
--- src/Play.Catalog.Service/Repositories/ItemsRepository.cs
+++ src/Play.Catalog.Service/Repositories/ItemsRepository.cs
@@ -10,10 +10,8 @@ namespace Play.Catalog.Service.Repositories
 
         private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
 
-        public ItemsRepository()
-        {
-            var mongoClient = new MongoClient("mongodb://localhost:27017");
-            var database = mongoClient.GetDatabase("Catalog");
+        public ItemsRepository(IMongoDatabase database)
+        {            
             this.dbCollection = database.GetCollection<Item>(collectionName);
         }
 
