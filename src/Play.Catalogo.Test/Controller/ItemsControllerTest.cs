93c4ba131fae96eb6892fe6033914e08d651c735
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-01-25 21:27:06 +0300

    add an integration test for itemscontroller get all items endpooint

---
+++ src/Play.Catalogo.Test/Controller/ItemsControllerTest.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Play.Catalog.Service.Dtos;
+using Xunit;
+
+namespace Play.Catalogo.Test.Controller
+{
+    public class ItemsControllerTest
+    {
+        private const string baseApiUrl = "/api/items";
+        private readonly HttpClient _httpClient;
+
+        public ItemsControllerTest()
+        {
+            var webAppFactory = new WebApplicationFactory<Program>();
+            _httpClient = webAppFactory.CreateDefaultClient();
+        }
+        [Fact]
+        public async void Get_AllItems()
+        {
+            var response = await _httpClient.GetFromJsonAsync<List<ItemDto>>(baseApiUrl);
+
+            Assert.NotNull(response);
+            Assert.Collection(response , item => Assert.Contains("Potion", item.Name));
+        }
+    }
+}
